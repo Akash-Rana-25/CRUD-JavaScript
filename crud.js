1aa5664a8734abf11cc47e5469638af88bb93e01
Akash-Rana-25 <akashrana.work.gmail.com>
2023-02-06 12:39:42 +0530

    - Validation messages are fired when Submit button is clicked
    - Once submit button is clicked, pop up should be hidden again
    
    - No Validation for File Upload control. Currently zip and rar files are also getting uploaded
    
    - Search is not working properly

--- crud.js
+++ crud.js
@@ -6,9 +6,9 @@ function display() {
         card += ` <div class="col shadow-sm p-3 mb-5 bg-body rounded">
         <img src="${newproducts.img}"style="height: 10rem;  width: 12rem;" class="card-img-top" alt="${newproducts.name}"></img> 
             <div class="card-body">
-                <h5 class="card-title">${newproducts.name}</h5>
-                <p class="card-text">${newproducts.price}</p>
-                <p class="card-text"><small class="text-muted"></small>${newproducts.description}</p>
+                <h5 class="card-title">Name : ${newproducts.name}</h5>
+                <p class="card-text">Price : ${newproducts.price}</p>
+                <p class="card-text"><small class="text-muted"></small>Description ${newproducts.description}</p>
                 <button type="button" id="update_btn" onclick="editProduct('${newproducts.id}')" class="btn btn-outline-success" data-bs-toggle="modal"
                     data-bs-target="#editModal">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -43,68 +43,120 @@ function addData() {
     const product_img = document.getElementById("product_img").files[0];
 
 
-    let reader = new FileReader();
-    reader.readAsDataURL(product_img);
-    reader.addEventListener("load", () => {
-        var url = reader.result;
-        const newproduct = {
-            id: product_id,
-            name: product_name,
-            price: product_price,
-            description: product_description,
-            img: url
+    var fileInput =
+        document.getElementById('product_img');
 
-        }
+    var filePath = fileInput.value;
 
-        var items = JSON.parse(localStorage.getItem("Product_Details"));
-        if (items === null) {
-            const products = JSON.parse(localStorage.getItem("Product_Details")) ?? [];
-            products.push(newproduct);
-            localStorage.setItem("Product_Details", JSON.stringify(products));
+    // Allowing file type
+    var allowedExtensions =
+        /(\.jpg|\.jpeg|\.png|\.gif|\.webp)$/i;
+    if (product_id === '' || product_img === '' || product_name === '' || product_price === '' || product_description === '') {
+        swal({
+            title: 'Please fill all the filed !!',
+            text: 'Redirecting...',
+            icon: 'error',
+            timer: 2000,
+            buttons: false,
+        })
+            .then(() => {
+                dispatch(redirect('/'));
+            })
+    } else {
+
+
+
+        if (!allowedExtensions.exec(filePath)) {
             swal({
-                title: 'Product Added',
+                title: 'Please Select .jpg ,.jpeg,.png,.gif Only !!',
                 text: 'Redirecting...',
-                icon: 'success',
-                timer: 3000,
+                icon: 'error',
+                timer: 2000,
                 buttons: false,
             })
                 .then(() => {
                     dispatch(redirect('/'));
                 })
-            display();
+            fileInput.value = '';
+            return false;
         } else {
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].id == product_id) {
+            let reader = new FileReader();
+            reader.readAsDataURL(product_img);
+            reader.addEventListener("load", () => {
+                var url = reader.result;
+                const newproduct = {
+                    id: product_id,
+                    name: product_name,
+                    price: product_price,
+                    description: product_description,
+                    img: url
+
+                }
+
+                var items = JSON.parse(localStorage.getItem("Product_Details"));
+                if (items === '') {
+                    const products = JSON.parse(localStorage.getItem("Product_Details")) ?? [];
+                    products.push(newproduct);
+                    localStorage.setItem("Product_Details", JSON.stringify(products));
+                    var myModalEl = document.getElementById('addModal');
+                    var modal = bootstrap.Modal.getInstance(myModalEl)
+                    modal.hide();
                     swal({
-                        title: 'ID alredy eixst',
+                        title: 'Product Added',
                         text: 'Redirecting...',
-                        icon: 'error',
+                        icon: 'success',
                         timer: 3000,
                         buttons: false,
                     })
-                    break;
+                        .then(() => {
+                            dispatch(redirect('/'));
+                        })
+
+                    display();
+                } else {
+
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].id == product_id) {
+                            console.log(items[i].id);
+                            console.log(product_id);
+
+                            swal({
+                                title: 'ID alredy eixst',
+                                text: 'Redirecting...',
+                                icon: 'error',
+                                timer: 3000,
+                                buttons: false,
+                            }).then(() => {
+                                dispatch(redirect('/'));
+                            })
+                            return;
+                        }
+                    }
+
+                    const products = JSON.parse(localStorage.getItem("Product_Details")) ?? [];
+                    products.push(newproduct);
+                    localStorage.setItem("Product_Details", JSON.stringify(products));
+                    var myModalEl = document.getElementById('addModal');
+                    var modal = bootstrap.Modal.getInstance(myModalEl)
+                    modal.hide();
+                    swal({
+                        title: 'Product Added',
+                        text: 'Redirecting...',
+                        icon: 'success',
+                        timer: 3000,
+                        buttons: false,
+                    })
+                    var frm = document.getElementsByName('myform')[0];
+                    frm.reset();  // Reset all form data
+                    display();
+                    return false; // Prevent page refresh
                 }
-            }
 
-            const products = JSON.parse(localStorage.getItem("Product_Details")) ?? [];
-            products.push(newproduct);
-            localStorage.setItem("Product_Details", JSON.stringify(products));
-            swal({
-                title: 'Product Added',
-                text: 'Redirecting...',
-                icon: 'success',
-                timer: 3000,
-                buttons: false,
-            })
-
-
-
-            display();
+            });
         }
-    });
-    var frm = document.getElementsByName('myform')[0];
-    frm.reset();  // Reset all form data
-    return false; // Prevent page refresh
+
+        display();
+    }
 }
 
 var inputBox = document.getElementById("product_price");
@@ -140,35 +192,60 @@ function editProduct(id) {
     const update_img = document.getElementById("update_product_img");
     document.querySelector("#submit_btn2").onclick = function () {
         if (update_img.value != "") {
-           
-            const update_product_img = document.getElementById("update_product_img").files[0];
+            var fileInput =
+                document.getElementById('update_product_img');
 
-            let reader = new FileReader();
-            reader.readAsDataURL(update_product_img);
-            reader.addEventListener("load", () => {
-                var url = reader.result;
-                items[index].id = document.getElementById("update_product_id").value;
-                items[index].name = document.getElementById("update_product_name").value;
-                items[index].price = document.getElementById("update_product_price").value;
-                items[index].description = document.getElementById("update_product_description").value;
-                items[index].img = url;
-                localStorage.setItem("Product_Details", JSON.stringify(items));
-                display();
-            });
+            var filePath = fileInput.value;
 
+            // Allowing file type
+            var allowedExtensions =
+                /(\.jpg|\.jpeg|\.png|\.gif|\.webp)$/i;
 
+            if (!allowedExtensions.exec(filePath)) {
+                swal({
+                    title: 'Please Select .jpg ,.jpeg,.png,.gif Only !!',
+                    text: 'Redirecting...',
+                    icon: 'error',
+                    timer: 2000,
+                    buttons: false,
+                })
+                    .then(() => {
+                        dispatch(redirect('/'));
+                    })
+                fileInput.value = '';
+                return false;
+            } else {
 
+                const update_product_img = document.getElementById("update_product_img").files[0];
 
-            swal({
-                title: 'Product Updated',
-                text: 'Redirecting...',
-                icon: 'success',
-                timer: 3000,
-                buttons: false,
-            })
+                let reader = new FileReader();
+                reader.readAsDataURL(update_product_img);
+                reader.addEventListener("load", () => {
+                    var url = reader.result;
+                    items[index].id = document.getElementById("update_product_id").value;
+                    items[index].name = document.getElementById("update_product_name").value;
+                    items[index].price = document.getElementById("update_product_price").value;
+                    items[index].description = document.getElementById("update_product_description").value;
+                    items[index].img = url;
+                    localStorage.setItem("Product_Details", JSON.stringify(items));
+
+                    display();
+                    var myModalEl = document.getElementById('editModal');
+                    var modal = bootstrap.Modal.getInstance(myModalEl)
+                    modal.hide();
+                });
+
+                swal({
+                    title: 'Product Updated',
+                    text: 'Redirecting...',
+                    icon: 'success',
+                    timer: 3000,
+                    buttons: false,
+                })
+            }
         }
-        else if(update_img.value === ""){
-         
+        else if (update_img.value === "") {
+
             items[index].id = document.getElementById("update_product_id").value;
             items[index].name = document.getElementById("update_product_name").value;
             items[index].price = document.getElementById("update_product_price").value;
@@ -177,6 +254,9 @@ function editProduct(id) {
 
             localStorage.setItem("Product_Details", JSON.stringify(items));
             display();
+            var myModalEl = document.getElementById('editModal');
+            var modal = bootstrap.Modal.getInstance(myModalEl)
+            modal.hide();
             swal({
                 title: 'Product Updated',
                 text: 'Redirecting...',
@@ -187,14 +267,17 @@ function editProduct(id) {
                 .then(() => {
                     dispatch(redirect('/'));
                 })
+            // document.getElementById('editModal').innerHTML = ''; 
             localStorage.setItem("Product_Details", JSON.stringify(items));
+
         }
-        else{
+        else {
             console.log("err");
         }
 
 
     }
+
 
 }
 
@@ -257,10 +340,11 @@ function searchProduct(searchKeyword) {
 
     let filteredProducts;
     var products = JSON.parse(localStorage["Product_Details"]);
+    // Object.keys(product).some(key => product[key].toLowerCase().includes(searchKeyword.toLowerCase())));
     if (searchKeyword === '') {
         filteredProducts = products;
     } else {
-        filteredProducts = products.filter((product) => Object.keys(product).some(key => product[key].toLowerCase().includes(searchKeyword.toLowerCase())));
+        filteredProducts = products.filter((product) => product["name"].toLowerCase().includes(searchKeyword.toLowerCase()) || product["price"].toLowerCase().includes(searchKeyword.toLowerCase()) || product["description"].toLowerCase().includes(searchKeyword.toLowerCase()) || product["id"].toLowerCase().includes(searchKeyword.toLowerCase()));
     }
 
     displayProducts(filteredProducts);
